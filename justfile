92aef03eedcfc892c88097582db8cc2084d9ba22
alex weidner <shimmerjs@dpu.sh>
2021-03-20 01:12:40 -0400

    add the homies

--- justfile
+++ justfile
@@ -1,3 +1,5 @@
+homies := "dakale scottshuffler booninite"
+
 build hostname=`hostname`:
   #!/usr/bin/env bash
   if [ {{os()}} == "macos" ]; then
@@ -21,3 +23,6 @@ switch hostname=`hostname`:
 
 update-hardware-configuration hostname=`hostname`:
   nixos-generate-config --show-hardware-config > machines/{{hostname}}/hardware/default.nix
+
+update-ssh-keys:
+  for homie in {{homies}}; do curl https://github.com/$homie.keys -o system/ssh/keys/$homie.keys; done
--- system/users/default.nix
+++ system/users/default.nix
@@ -8,4 +8,18 @@
     home = "/home/shimmerjs";
     shell = pkgs.zsh;
   };
+
+  users.users.scott = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
+    home = "/home/scott";
+    shell = pkgs.zsh;
+  };
+
+  users.users.david = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
+    home = "/home/david";
+    shell = pkgs.zsh;
+  };
 }
