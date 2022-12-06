10148641345ceebe3f5883bf58745e184ab854f7
alex weidner <shimmerjs@dpu.sh>
2022-12-05 19:50:49 -0500

    o

--- homies/shimmerjs/machines/herq/default.nix
+++ homies/shimmerjs/machines/herq/default.nix
@@ -1,4 +1,10 @@
 { config, lib, pkgs, ... }:
+let
+  # import raw sources to use github sources
+  sources = import <u/nix/sources.nix>;
+  # import niv-managed pkgs overlay
+  pkgs = import <u/nix> { inherit sources; };
+in
 {
   imports = [
     <home-manager/nixos>
