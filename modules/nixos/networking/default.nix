4e36749ad7ddfbe4d452d3694c43301766fc3897
alex weidner <shimmerjs@dpu.sh>
2022-12-05 19:35:11 -0500

    o

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -5,4 +5,5 @@
   services.tailscale.enable = true;
   networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
   networking.search = [ "dpu.sh.beta.tailscale.net" ];
+  networking.firewall.checkReversePath = "loose";
 }
