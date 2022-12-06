91159ff8afa1780d36f28f01b88c64a3234d6d97
alex weidner <shimmerjs@dpu.sh>
2022-12-05 19:28:47 -0500

    update user settings for latest nixos

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -8,9 +8,9 @@
 
   # i am always allow to do nix stuff on my own machines
   # TODO: parameterize (module?)
-  nix = {
-    allowedUsers = [ "shimmerjs" ];
-    trustedUsers = [ "root" "shimmerjs" ];
+  settings = {
+    allowed-users = [ "shimmerjs" ];
+    trusted-users = [ "root" "shimmerjs" ];
   };
 
   time.timeZone = "America/New_York";
