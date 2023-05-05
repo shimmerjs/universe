5d042f197da2212698ccd4427c3df72d4b82c36f
alex weidner <aw185176@ncr.com>
2023-05-05 16:19:23 -0400

    add new machine

---
+++ homies/shimmerjs/machines/WUSAW185176-E9J/home.nix
@@ -0,0 +1,21 @@
+# NCR workbook
+{ config, ... }:
+let
+  # import raw sources to use github sources
+  sources = import <u/nix/sources.nix>;
+  # import niv-managed pkgs overlay
+  pkgs = import <u/nix> { inherit sources; };
+in
+{
+  imports = [
+    <u/modules/home-manager>
+    <u/modules/home-manager/osx>
+    <u/modules/home-manager/ssh>
+    <u/homies/shimmerjs/home-manager.nix>
+    <u/homies/shimmerjs/ssh.nix>
+    <u/homies/shimmerjs/profiles/work>
+  ];
+
+  # set up machine specific kitty ssh-conf
+  home.file.".config/kitty/ssh.conf".source = ./ssh.conf;
+}
