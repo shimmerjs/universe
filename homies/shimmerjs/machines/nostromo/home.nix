e6dda3f72cfb72dff1d275424c1a2cd80825612b
alex weidner <shimmerjs@dpu.sh>
2024-01-22 21:00:28 -0500

    add new macbook

---
+++ homies/shimmerjs/machines/nostromo/home.nix
@@ -0,0 +1,26 @@
+# personal crackbook
+{ config, ... }:
+let
+  # import raw sources to use github sources
+  sources = import <u/nix/sources.nix>;
+  # import niv-managed pkgs overlay
+  pkgs = import <u/nix> { inherit sources; };
+in
+{
+  imports = [
+    <u/modules/home-manager>
+    <u/modules/home-manager/osx>
+    <u/homies/shimmerjs/profiles/play>
+    <u/homies/shimmerjs/ssh.nix>
+    <u/homies/shimmerjs/home-manager.nix>
+  ];
+
+  programs.ssh.extraConfig = ''
+    Host github.com
+      User git
+      Port 22
+      HostName github.com
+      PreferredAuthentications publickey
+      IdentityFile /Users/shimmerjs/.ssh/booninite
+  '';
+}
